c8106c1ccff4c90f34f06a9a67f90e6b47060085
Lavínia Rodrigues <lavinia.rodriguesr@gmail.com>
2024-07-11 10:36:32 -0300

    Initial commit

---
+++ src/test/java/r/lavinia/ApiRestDioApplicationTests.java
@@ -0,0 +1,13 @@
+package r.lavinia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
